dbbe427c9e08b8e0bc3d2704290b158e7293be42
yunusyildiz-ch <yunusyildizch@gmail.com>
2025-04-04 22:34:37 +0200

    build:  🔥 Set up Firebase and Firestore integration with working test write/read functions

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import TestFirebase from "./TestFirebase";
+// function App() {
+//   return (
+//     <div className="p-4">
+//       <h1>Firebase Manual Test</h1>
+//       <TestFirebase />
+//     </div>
+//   );
+// }
+
+// export default App;
