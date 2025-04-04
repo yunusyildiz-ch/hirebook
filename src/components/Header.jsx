047284915d238229e7d4857c6d2ca1e6f561ae19
yunusyildiz-ch <yunusyildizch@gmail.com>
2025-04-04 20:55:04 +0200

    🏷️ Add filtering and tagging system to Candidates page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import ThemeToggle from "./ThemeToggle";
+
 const Header = () => {
-    return (
-      <header className="bg-white dark:bg-gray-900 shadow p-4">
-        <h1 className="text-xl font-semibold">Dashboard</h1>
-      </header>
-    );
-  };
-  
-  export default Header;
+  return (
+    <header className="bg-white dark:bg-gray-900 shadow p-4 flex justify-between items-center">
+      <h1 className="text-xl font-semibold">dashboard</h1>
+
+      {/* Toggle sağda */}
+      <ThemeToggle />
+    </header>
+  );
+};
+
+export default Header;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,4 @@
+import { createBrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import Notes from "./pages/Notes";
 import Candidates from "./pages/Candidates";
