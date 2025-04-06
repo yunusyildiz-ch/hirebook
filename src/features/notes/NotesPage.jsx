7851eae6c1e7850c5e273c398420be15e00294b4
yunusyildiz-ch <yunusyildizch@gmail.com>
2025-04-06 16:41:09 +0200

    update: notespage

--- src/features/notes/NotesPage.jsx
+++ src/features/notes/NotesPage.jsx
@@ -1,15 +1,19 @@
 import { useSelector } from "react-redux";
 import AllNotes from "./views/AllNotes";
-import NewNote from "./views/NewNote";
-import FolderView from "./views/FolderView";
-
-const views = {
-  All: <AllNotes />,
-  New: <NewNote />,
-  Folders: <FolderView />,
-};
+import NoteDetail from "../notes/components/NoteDetail";
+import NoteEditor from "../notes/components/NoteEditor";
 
 export default function NotesPage() {
-  const activeTab = useSelector((state) => state.notes.activeTab);
-  return <div className="space-y-6">{views[activeTab] || <AllNotes />}</div>;
+  const viewMode = useSelector((state) => state.notes.viewMode);
+
+  if (viewMode === "view") {
+    return <NoteDetail />;
+  }
+
+  if (viewMode === "edit") {
+    return <NoteEditor />;
+  }
+
+  // Default is list mode
+  return <AllNotes />;
 }
